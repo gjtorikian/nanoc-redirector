ab40d57dc3d65ef903fac59219d1aeea1acaacae
joerggollnick <github@wurzelbenutzer.de>
2018-10-02 23:03:18 +0200

    Use @config instead of hardcoded values
    Use config[:output_dir] (output_dir) instead of output
    Use config[:index_filenames][0] (first index filename) instead of index.html

--- lib/nanoc-redirector.rb
+++ lib/nanoc-redirector.rb
@@ -3,10 +3,12 @@ require 'version'
 
 module NanocRedirector
   module RedirectFrom
-    def self.process(item, dest)
+    def self.process(item, dest, config)
       return if item[:redirect_from].nil?
       return if dest.nil?
       redirect_hash = {}
+      output_dir = config[:output_dir]
+      index_file = config[:index_filenames][0]
 
       key = item.identifier.without_ext
       value = item[:redirect_from].is_a?(String) ? [item[:redirect_from]] : item[:redirect_from]
@@ -16,8 +18,8 @@ module NanocRedirector
       redirect_hash.values.each do |redirects|
         redirects.each do |redirect|
           content = NanocRedirector.redirect_template(dest)
-          dir = File.join("output", redirect)
-          redirect_path = File.join(dir, "index.html")
+          dir = File.join(output_dir, redirect)
+          redirect_path = File.join(dir, index_file)
           FileUtils.mkdir_p(dir) unless File.directory?(dir)
           File.write(redirect_path, content) unless File.exist? redirect_path
         end
