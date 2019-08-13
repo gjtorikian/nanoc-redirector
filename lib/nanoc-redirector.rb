2cc5da314e4d180c2097ca893211f166316bb695
joerggollnick <github+nanoc-redirector@wurzelbenutzer.de>
2019-08-13 20:24:07 +0200

    RedirectFrom config optional untested

--- lib/nanoc-redirector.rb
+++ lib/nanoc-redirector.rb
@@ -3,12 +3,17 @@ require 'version'
 
 module NanocRedirector
   module RedirectFrom
-    def self.process(item, dest, config)
+    def self.process(item, dest, config = nil)
       return if item[:redirect_from].nil?
       return if dest.nil?
       redirect_hash = {}
-      output_dir = config[:output_dir]
-      index_file = config[:index_filenames][0]
+      if config.nil?
+        output_dir = "output"
+        index_filename = "index.html"
+      else
+        output_dir     = config[:output_dir]
+        index_filename = config[:index_filenames][0]
+      end
 
       key = item.identifier.without_ext
       value = item[:redirect_from].is_a?(String) ? [item[:redirect_from]] : item[:redirect_from]
@@ -19,7 +24,7 @@ module NanocRedirector
         redirects.each do |redirect|
           content = NanocRedirector.redirect_template(dest)
           dir = File.join(output_dir, redirect)
-          redirect_path = File.join(dir, index_file)
+          redirect_path = File.join(dir, index_filename)
           FileUtils.mkdir_p(dir) unless File.directory?(dir)
           File.write(redirect_path, content) unless File.exist? redirect_path
         end
